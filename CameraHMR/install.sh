0cbda6dfd6a370f418e3753b090bb41263e018ce
weichenMT <zhangweichen2006@gmail.com>
2025-09-15 09:47:57 +0800

    train

--- CameraHMR/install.sh
+++ CameraHMR/install.sh
@@ -2,7 +2,8 @@ git clone https://github.com/open-mmlab/mmcv.git
 cd mmcv
 git checkout v1.3.9
 MMCV_WITH_OPS=1 pip install -e .
-sudo apt install libjpeg-turbo8 libjpeg-turbo-progs libosmesa6 libosmesa6-dev mesa-utils-extra libgl1-mesa-dri
+apt install libjpeg-turbo8 libjpeg-turbo-progs libosmesa6 libosmesa6-dev mesa-utils-extra libgl1-mesa-dri
+apt-get install libturbojpeg
 
 apt install libglvnd0 libegl1 libgles2
 apt install -y libegl-dev libgles2-mesa-dev
