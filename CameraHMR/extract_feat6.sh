d321a9610b6843ad695710cf6558bb1f6bcc84cc
weichenMT <zhangweichen2006@gmail.com>
2025-09-24 10:38:28 +0800

    minor

--- CameraHMR/extract_feat6.sh
+++ CameraHMR/extract_feat6.sh
@@ -1,6 +1,9 @@
 CUDA_VISIBLE_DEVICES=6 python SapiensLite/demo/vis_normal.py \
     SapiensLite/torchscript/normal/checkpoints/sapiens_2b/sapiens_2b_normal_render_people_epoch_70_torchscript.pt2 \
-    --input data/training-images \
-    --output_root data/training-images-sapiens-normals-DirectResize \
+    --input data/training-images/20221011_1_250_batch01hand_closeup_suburb_a_6fps \
+    --output_root data/training-images-sapiens-normals-DirectResize-local/20221011_1_250_batch01hand_closeup_suburb_a_6fps \
+    --output_imgmatch_root data/training-images-sapiens-normals-DirectResize-imgmatch-local/20221011_1_250_batch01hand_closeup_suburb_a_6fps \
     --preprocess resize \
+    --swapHW \
+    --redo \
     --batch_size 1
