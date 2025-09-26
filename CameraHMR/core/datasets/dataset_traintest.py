110c04e67d24b16916d8b565e697efcb3a0eedb2
WeichenMAC <zhangweichen2006@gmail.com>
2025-09-27 09:37:20 +1000

    rcv

--- CameraHMR/core/datasets/dataset_traintest.py
+++ CameraHMR/core/datasets/dataset_traintest.py
@@ -303,17 +303,17 @@ class DatasetTrainTest(Dataset):
                     np.save(sapiens_normals_path_to_regenerate_imgmatch[idx], rev_normal_imgmatch)
                     valid_paths_sapiens_normals.append(True)
                     cv2.imwrite(sapiens_normals_path_to_regenerate_imgmatch[idx].replace('.npy', '.png'), 255*(rev_normal_imgmatch*0.5+0.5))
-                    # os.system(f"mkdir -p {os.path.dirname(self.sapiens_normals_path2[idx])}")
-                    # os.system(f"mv {self.sapiens_normals_path[idx]} {self.sapiens_normals_path2[idx]} &")
+                    os.system(f"mkdir -p {os.path.dirname(self.sapiens_normals_path2[idx])}")
+                    os.system(f"mv {self.sapiens_normals_path[idx]} {self.sapiens_normals_path2[idx]} &")
                 else:
                     valid_paths_sapiens_normals.append(False)
             else:
                 valid_paths_sapiens_normals.append(True)
                 # move normal path 1 (vepfs local) to normal path 2 (tos)
                 log.info(f"IMGMatch normal exists. moving {self.sapiens_normals_path[idx]} to {self.sapiens_normals_path2[idx]}")
-                # os.system(f"mkdir -p {os.path.dirname(self.sapiens_normals_path2[idx])}")
-                # os.system(f"mv {self.sapiens_normals_path[idx]} {self.sapiens_normals_path2[idx]} &")
-                # os.system(f"mv {self.sapiens_normals_path[idx].replace('.npy', '.png')} {self.sapiens_normals_path2[idx].replace('.npy', '.png')} &")
+                os.system(f"mkdir -p {os.path.dirname(self.sapiens_normals_path2[idx])}")
+                os.system(f"mv {self.sapiens_normals_path[idx]} {self.sapiens_normals_path2[idx]} &")
+                os.system(f"mv {self.sapiens_normals_path[idx].replace('.npy', '.png')} {self.sapiens_normals_path2[idx].replace('.npy', '.png')} &")
         valid_paths_sapiens_normals = np.array(valid_paths_sapiens_normals)
 
         if not valid_paths_sapiens_normals.all():
